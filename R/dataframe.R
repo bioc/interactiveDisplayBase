7507a3992fcd13a6c1966fd1991b4a41e07d8748
Martin Morgan <mtmorgan@fhcrc.org>
2015-09-12 01:35:57 +0000

    missing open paren
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/interactiveDisplayBase@108454 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/dataframe.R
+++ R/dataframe.R
@@ -79,7 +79,7 @@
     })
     ## selectively use the RStudio viewer pane (if available)
     viewer <- getOption("viewer")
-    if (!is.null(viewer) && is.function(viewer))
+    if (!is.null(viewer) && is.function(viewer)) {
         runApp(app, launch.browser = viewer, ...)
     }else{
         runApp(app, ...)
