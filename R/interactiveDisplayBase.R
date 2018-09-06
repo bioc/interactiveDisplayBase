d0ed4d9cd53cc6a59ae6de4a9c71b07451954d7e
LiNk-NY <marcel.ramosperez@roswellpark.org>
2018-09-06 15:51:44 +0000

    additional updates from BiocInstaller to BiocManager

--- R/interactiveDisplayBase.R
+++ R/interactiveDisplayBase.R
@@ -28,7 +28,7 @@ function(object){
       install.packages("BiocManager")
   if (!is.element(p, installed.packages()[,1])){
     stop(paste("The required package, '",p,"', is missing.  Please install it by
-               typing biocLite('",p,"') in the console", sep=""))
+               typing BiocManager::install('",p,"') in the console", sep=""))
   }
   require(p, character.only = TRUE)
 }
